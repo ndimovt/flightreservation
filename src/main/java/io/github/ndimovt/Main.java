643df1859dbc016e59ea02894978ca7951762fa1
ndimovt <nikolaitodorov119@gmail.com>
2023-12-30 20:00:14 +0200

    -created UserMenu.class -added planeChoice() to UserMenu.class to choose planes in random order -added menuChoice() to UserMenu.class to choose seats and to add record -updated main()

--- src/main/java/io/github/ndimovt/Main.java
+++ src/main/java/io/github/ndimovt/Main.java
@@ -1,47 +1,8 @@
 package io.github.ndimovt;
 
-import io.github.ndimovt.planes.AirbusA220;
-import io.github.ndimovt.planes.Plane;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner inn = new Scanner(System.in);
-        Plane airbusA220 = new AirbusA220();
-        airbusA220.read();
-        boolean isTrue = true;
-        while (isTrue) {
-            System.out.println("1) Reserve place 2) Check passenger list 3) Exit");
-            int choice = inn.nextInt();
-            inn.nextLine();
-            switch (choice){
-                case 1:
-                    System.out.println("Enter seat");
-                    String seat = inn.nextLine();
-                    if (FlightInformation.getRes().containsKey(seat)) {
-                        System.out.println("Seat already reserved! Please chose another one.");
-                    }else {
-                        SeatReservation seatReservation = new SeatReservation(seat);
-                        Thread t = new Thread(seatReservation);
-                        t.start();
-                        try {
-                            Thread.sleep(100000);
-                        } catch (InterruptedException ie) {
-                            ie.printStackTrace();
-                        }
-                    }
-                    break;
-                case 2:
-                    System.out.println(FlightInformation.getRes().entrySet());
-                    break;
-                case 3:
-                    isTrue = false;
-                    break;
-                default:
-                    System.out.println("Please enter valid number.");
-                    break;
-            }
-        }
+        UserMenu menu = new UserMenu();
+        menu.menuChoice();
     }
 }
---
+++ src/main/java/io/github/ndimovt/UserMenu.java
@@ -0,0 +1,65 @@
+package io.github.ndimovt;
+
+import io.github.ndimovt.planes.AirbusA220;
+import io.github.ndimovt.planes.AirbusA320;
+import io.github.ndimovt.planes.Plane;
+
+import java.util.Scanner;
+
+public class UserMenu {
+    private Scanner inn = new Scanner(System.in);
+    private Plane plane;
+    public void menuChoice(){
+        planeChoice();
+        boolean isTrue = true;
+        while (isTrue) {
+            System.out.println("1) Reserve place 2) Check passenger list 3) Exit");
+            int choice = inn.nextInt();
+            inn.nextLine();
+            switch (choice){
+                case 1:
+                    System.out.println("Enter seat");
+                    String seat = inn.nextLine();
+                    if (FlightInformation.getRes().containsKey(seat)) {
+                        System.out.println("Seat already reserved! Please chose another one.");
+                    }else {
+                        SeatReservation seatReservation = new SeatReservation(seat);
+                        Thread t = new Thread(seatReservation);
+                        t.start();
+                        try {
+                            Thread.sleep(100000);
+                        } catch (InterruptedException ie) {
+                            ie.printStackTrace();
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println(FlightInformation.getRes().entrySet());
+                    break;
+                case 3:
+                    isTrue = false;
+                    break;
+                default:
+                    System.out.println("Please enter valid number.");
+                    break;
+            }
+        }
+    }
+    private void planeChoice(){
+        int num = (int) (Math.random()*2);
+        switch (num){
+            case 0:
+                plane = new AirbusA220();
+                plane.read();
+                break;
+            case 1:
+                plane = new AirbusA320();
+                plane.read();
+                break;
+            default:
+                plane = new Plane();
+                plane.read();
+                break;
+        }
+    }
+}
